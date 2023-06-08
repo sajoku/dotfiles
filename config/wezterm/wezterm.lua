ceb8e188ddc6ea5977582890f6f7e1105d28be7a
sander <sander@muliphen.nl>
2023-06-08 09:44:49 +0200

    Just do Custom font as Wezterm now has built in fonts for icons
    - Yeay, this is simpler.

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -2,15 +2,14 @@ local wezterm = require("wezterm")
 
 function scheme_for_appearance(appearance)
 	if appearance:find("Dark") then
-		return "rose-pine-moon"
+		return "Catppuccin Mocha"
 	else
-		--return "Rosé Pine Dawn (base16)"
-		return "rose-pine-moon"
+		return "Catppuccin Frappe"
 	end
 end
 
 return {
-	font = wezterm.font("MonoLisa Nerd Font"),
+	font = wezterm.font("MonoLisa Custom"),
 	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 	hide_tab_bar_if_only_one_tab = true,
 
