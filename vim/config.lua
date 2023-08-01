4591e59ec8b8872d55392793e37f79c770042804
sander <sander@muliphen.nl>
2023-08-01 13:58:40 +0200

    Only use the builtin lsp
    - This has the drawback that we need to install lsp servers manually but has the benefit of being much more transparent in the config and is closer to how i would like to see this work.
    - This does not take care of all the things I had. ie. code-formatting for python but that might be due to ruff_lsp not being updated yet

--- vim/config.lua
+++ vim/config.lua
@@ -1,219 +1,207 @@
 require("nvim-treesitter.configs").setup({
-	-- A list of parser names, or "all"
-	ensure_installed = { "c", "lua", "rust", "ruby", "python", "json", "vim", "yaml", "html", "css" },
+  -- A list of parser names, or "all"
+  ensure_installed = { "c", "lua", "rust", "ruby", "python", "json", "vim", "yaml", "html", "css" },
 
-	-- Install parsers synchronously (only applied to `ensure_installed`)
-	sync_install = false,
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
 
-	auto_install = true,
-	-- List of parsers to ignore installing (for "all")
-	--ignore_install = { "javascript" },
+  auto_install = true,
+  -- List of parsers to ignore installing (for "all")
+  --ignore_install = { "javascript" },
 
-	highlight = {
-		enable = true,
-		additional_vim_regex_highlighting = false,
-	},
-	endwise = {
-		enable = true,
-	},
-	indent = {
-		enable = true,
-	},
+  highlight = {
+    enable = true,
+    additional_vim_regex_highlighting = false,
+  },
+  endwise = {
+    enable = true,
+  },
+  indent = {
+    enable = true,
+  },
 })
 
 require("treesitter-context").setup({
-	enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-	max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-	trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-	min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
-	patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-		-- For all filetypes
-		-- Note that setting an entry here replaces all other patterns for this entry.
-		-- By setting the 'default' entry below, you can control which nodes you want to
-		-- appear in the context window.
-		default = {
-			"class",
-			"function",
-			"method",
-			"for",
-			"while",
-			"if",
-			"switch",
-			"case",
-		},
-		-- Patterns for specific filetypes
-		-- If a pattern is missing, *open a PR* so everyone can benefit.
-		tex = {
-			"chapter",
-			"section",
-			"subsection",
-			"subsubsection",
-		},
-		rust = {
-			"impl_item",
-			"struct",
-			"enum",
-		},
-		scala = {
-			"object_definition",
-		},
-		vhdl = {
-			"process_statement",
-			"architecture_body",
-			"entity_declaration",
-		},
-		markdown = {
-			"section",
-		},
-		elixir = {
-			"anonymous_function",
-			"arguments",
-			"block",
-			"do_block",
-			"list",
-			"map",
-			"tuple",
-			"quoted_content",
-		},
-		json = {
-			"pair",
-		},
-		yaml = {
-			"block_mapping_pair",
-		},
-	},
-	exact_patterns = {
-		-- Example for a specific filetype with Lua patterns
-		-- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
-		-- exactly match "impl_item" only)
-		-- rust = true,
-	},
+  enable = true,         -- Enable this plugin (Can be enabled/disabled later via commands)
+  max_lines = 0,         -- How many lines the window should span. Values <= 0 mean no limit.
+  trim_scope = "outer",  -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+  min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+  patterns = {           -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+    -- For all filetypes
+    -- Note that setting an entry here replaces all other patterns for this entry.
+    -- By setting the 'default' entry below, you can control which nodes you want to
+    -- appear in the context window.
+    default = {
+      "class",
+      "function",
+      "method",
+      "for",
+      "while",
+      "if",
+      "switch",
+      "case",
+    },
+    -- Patterns for specific filetypes
+    -- If a pattern is missing, *open a PR* so everyone can benefit.
+    tex = {
+      "chapter",
+      "section",
+      "subsection",
+      "subsubsection",
+    },
+    rust = {
+      "impl_item",
+      "struct",
+      "enum",
+    },
+    scala = {
+      "object_definition",
+    },
+    vhdl = {
+      "process_statement",
+      "architecture_body",
+      "entity_declaration",
+    },
+    markdown = {
+      "section",
+    },
+    elixir = {
+      "anonymous_function",
+      "arguments",
+      "block",
+      "do_block",
+      "list",
+      "map",
+      "tuple",
+      "quoted_content",
+    },
+    json = {
+      "pair",
+    },
+    yaml = {
+      "block_mapping_pair",
+    },
+  },
+  exact_patterns = {
+    -- Example for a specific filetype with Lua patterns
+    -- Treat patterns.rust as a Lua pattern (i.e "^impl_item$" will
+    -- exactly match "impl_item" only)
+    -- rust = true,
+  },
 
-	-- [!] The options below are exposed but shouldn't require your attention,
-	--     you can safely ignore them.
+  -- [!] The options below are exposed but shouldn't require your attention,
+  --     you can safely ignore them.
 
-	zindex = 20, -- The Z-index of the context window
-	mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
-	-- Separator between context and content. Should be a single character string, like '-'.
-	-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
-	separator = nil,
+  zindex = 20,     -- The Z-index of the context window
+  mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+  -- Separator between context and content. Should be a single character string, like '-'.
+  -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+  separator = nil,
 })
 
--- Mappings.
+-- Autoformatting with LSP
+local async_formatting = function(bufnr)
+  bufnr = bufnr or vim.api.nvim_get_current_buf()
+
+  vim.lsp.buf_request(
+    bufnr,
+    "textDocument/formatting",
+    vim.lsp.util.make_formatting_params({}),
+    function(err, res, ctx)
+      if err then
+        local err_msg = type(err) == "string" and err or err.message
+        -- you can modify the log message / level (or ignore it completely)
+        vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
+        return
+      end
+
+      -- don't apply results if buffer is unloaded or has been modified
+      if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
+        return
+      end
+
+      if res then
+        local client = vim.lsp.get_client_by_id(ctx.client_id)
+        vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
+        vim.api.nvim_buf_call(bufnr, function()
+          vim.cmd("silent noautocmd update")
+        end)
+      end
+    end
+  )
+end
+
+-- Setup language servers.
+-- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+local lspconfig = require("lspconfig")
+
+lspconfig.luau_lsp.setup({})
+lspconfig.lua_ls.setup {}
+lspconfig.tailwindcss.setup({})
+lspconfig.marksman.setup({})
+lspconfig.html.setup({})
+
+lspconfig.pyright.setup({})
+local on_attach_ruff_lsp = function(client, bufnr)
+  -- Disable hover in favor of Pyright
+  client.server_capabilities.hoverProvider = false
+end
+lspconfig.ruff_lsp.setup({
+  on_attach = on_attach_ruff_lsp,
+})
+lspconfig.tsserver.setup({})
+lspconfig.standardrb.setup({
+})
+lspconfig.solargraph.setup {}
+lspconfig.rust_analyzer.setup({
+  -- Server-specific settings. See `:help lspconfig-setup`
+  settings = {
+    ["rust-analyzer"] = {},
+  },
+})
+
+-- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap = true, silent = true }
-vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
-vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, opts)
-vim.keymap.set("n", "]d", vim.diagnostic.goto_next, opts)
-vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist, opts)
+vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
-require("mason").setup()
-require("null-ls").setup({
-	sources = {
-		require("null-ls").builtins.formatting.stylua,
-		require("null-ls").builtins.diagnostics.eslint,
-		require("null-ls").builtins.completion.spell,
-		require("null-ls").builtins.diagnostics.standardrb,
-		require("null-ls").builtins.formatting.standardrb,
-		require("null-ls").builtins.diagnostics.ruff,
-		require("null-ls").builtins.formatting.ruff,
-	},
-})
-require("mason-null-ls").setup({
-	ensure_installed = { "jq", "standardrb", "isort", "mypy", "prettier", "ruff" },
-	automatic_installation = true,
-})
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+  callback = function(ev)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-local lsp_formatting = function(bufnr)
-	vim.lsp.buf.format({
-		bufnr = bufnr,
-		filter = function(client)
-			-- apply whatever logic you want (in this example, we'll only use null-ls)
-			-- This makes sure we only format whe null-ls is the lsp provided
-			return client.name == "null-ls"
-		end,
-	})
-end
-
--- if you want to set up formatting on save, you can use this as a callback
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
-local on_attach = function(client, bufnr)
-	-- Check if we can autoformat
-	if client.server_capabilities.documentFormattingProvider then
-		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-		vim.api.nvim_create_autocmd("BufWritePre", {
-			group = augroup,
-			buffer = bufnr,
-			callback = function()
-				lsp_formatting(bufnr)
-			end,
-		})
-	end
-
-	-- Enable completion triggered by <c-x><c-o>
-	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
-
-	-- Mappings.
-	-- See `:help vim.lsp.*` for documentation on any of the below functions
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
-	vim.keymap.set("n", "gR", vim.lsp.buf.rename, bufopts)
-	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-	vim.keymap.set("n", "<space>F", function()
-		vim.lsp.buf.format({ async = false })
-	end, bufopts)
-	vim.api.nvim_create_autocmd("CursorHold", {
-		buffer = bufnr,
-		callback = function()
-			local opts_ = {
-				focusable = false,
-				close_events = { "BufLeave", "CursorMoved", "InsertEnter", "FocusLost" },
-				border = "rounded",
-				source = "always",
-				prefix = " ",
-				scope = "cursor",
-			}
-			vim.diagnostic.open_float(nil, opts_)
-		end,
-	})
-end
-
-local mason_lspconfig = require("mason-lspconfig")
-mason_lspconfig.setup({
-	ensure_installed = {
-		"solargraph",
-		"rust_analyzer",
-		"marksman",
-		"tsserver",
-		"html",
-		"glint",
-		"dockerls",
-		"bashls",
-		"cucumber_language_server",
-		"sqlls",
-		"taplo",
-		"tailwindcss",
-		"terraformls",
-		"yamlls",
-	},
-	automatic_installation = true,
+    -- Buffer local mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local opts = { buffer = ev.buf }
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+    vim.keymap.set("n", "<space>wl", function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, opts)
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+    vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+    vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+    vim.keymap.set("n", "<space>f", function()
+      vim.lsp.buf.format({ async = true })
+    end, opts)
+  end,
 })
 
--- Set up completion using nvim_cmp with LSP source
-local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
-mason_lspconfig.setup_handlers({
-	function(server_name)
-		require("lspconfig")[server_name].setup({
-			on_attach = on_attach,
-			capabilities = capabilities,
-		})
-	end,
+-- On save format the code
+vim.api.nvim_create_autocmd("BufWritePre", {
+  buffer = buffer,
+  callback = function()
+    vim.lsp.buf.format { async = false }
+  end
 })
