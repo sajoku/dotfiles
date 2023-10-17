5072e01658a1100198494c83c215501022f65e3c
sander <sander@muliphen.nl>
2023-10-17 14:23:52 +0200

    Add quick CMD+key commands to switch tmux windows

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,23 +1,45 @@
 local wezterm = require("wezterm")
 
 function scheme_for_appearance(appearance)
-	if appearance:find("Dark") then
-		return "Catppuccin Mocha"
-	else
-		return "Catppuccin Macchiato"
-	end
+  -- Macchiato, Frappe, Late, Mocha
+  if appearance:find("Dark") then
+    return "Catppuccin Macchiato"
+    --return require('lua/rose-pine-moon').colors()
+  else
+    --return require('lua/rose-pine-moon').colors()
+    return "Catppuccin Frappe"
+  end
 end
 
+local act = wezterm.action
 return {
-	font = wezterm.font("MonoLisa Variable"),
-	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
-	hide_tab_bar_if_only_one_tab = true,
+  --font = wezterm.font("MonoLisa Variable"),
+  --font = wezterm.font("CommitMono"),
+  font = wezterm.font("MonoLisa Variable", { stretch = 'UltraCondensed' }),
+  color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
+  --colors = scheme_for_appearance(wezterm.gui.get_appearance()),
+  hide_tab_bar_if_only_one_tab = true,
 
-	keys = {
-		{
-			key = "n",
-			mods = "SHIFT|CTRL",
-			action = wezterm.action.ToggleFullScreen,
-		},
-	},
+  keys = {
+    {
+      key = "n",
+      mods = "SHIFT|CTRL",
+      action = wezterm.action.ToggleFullScreen,
+    },
+    {
+      key = '1', mods = 'CMD', action = act.SendString "\x13\x31"
+    },
+    {
+      key = '2', mods = 'CMD', action = act.SendString "\x13\x32"
+    },
+    {
+      key = '3', mods = 'CMD', action = act.SendString "\x13\x33"
+    },
+    {
+      key = '4', mods = 'CMD', action = act.SendString "\x13\x34"
+    },
+    {
+      key = '5', mods = 'CMD', action = act.SendString "\x13\x35"
+    },
+  },
 }
