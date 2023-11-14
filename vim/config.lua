8744d275f6248ffaa64d4ee0669bf7613f5e414c
sander <sander@muliphen.nl>
2023-11-14 09:52:43 +0100

    Add cmp configuration
    - This enables completion with functions and arguments

--- vim/config.lua
+++ vim/config.lua
@@ -136,6 +136,65 @@ local async_formatting = function(bufnr)
   )
 end
 
+-- Completions with nvim-cmp
+local cmp = require 'cmp'
+
+cmp.setup({
+  snippet = {
+    -- REQUIRED - you must specify a snippet engine
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+    end,
+  },
+  window = {
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' }, -- For luasnip users.
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype('gitcommit', {
+  sources = cmp.config.sources({
+    { name = 'git' }, -- You can specify the `git` source if [you were installed it](https://github.com/petertriho/cmp-git).
+  }, {
+    { name = 'buffer' },
+  })
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ '/', '?' }, {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = {
+    { name = 'buffer' }
+  }
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    { name = 'cmdline' }
+  })
+})
+
+-- Set up lspconfig.
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 -- Setup language servers.
 -- https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
 local lspconfig = require('lspconfig')
@@ -147,7 +206,9 @@ lspconfig.marksman.setup({})
 lspconfig.html.setup({})
 lspconfig.cssls.setup {}
 
-lspconfig.pyright.setup({})
+lspconfig.pyright.setup({
+  capabilities = capabilities,
+})
 local on_attach_ruff_lsp = function(client, bufnr)
   -- Disable hover in favor of Pyright
   client.server_capabilities.hoverProvider = false
@@ -158,9 +219,16 @@ lspconfig.ruff_lsp.setup({
   }
 })
 
-lspconfig.tsserver.setup({})
-lspconfig.standardrb.setup({
+--completions.completeFunctionCalls
+lspconfig.tsserver.setup({
+  capabilities = capabilities,
+  settings = {
+    completions = {
+      completeFunctionCalls = true
+    }
+  }
 })
+lspconfig.standardrb.setup({})
 lspconfig.solargraph.setup {}
 lspconfig.rust_analyzer.setup({
   -- Server-specific settings. See `:help lspconfig-setup`
