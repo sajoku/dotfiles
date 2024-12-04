97fd2c1d3d6c13b0cfed9f2f5b2dedaa8359a737
sander <sander@muliphen.nl>
2024-12-04 15:44:08 +0100

    Add kanagawa

--- vim/config.lua
+++ vim/config.lua
@@ -75,12 +75,39 @@ require('lualine').setup {
   }
 }
 
+-- Default options:
+require('kanagawa').setup({
+  compile = false,  -- enable compiling the colorscheme
+  undercurl = true, -- enable undercurls
+  commentStyle = { italic = true },
+  functionStyle = {},
+  keywordStyle = { italic = true },
+  statementStyle = { bold = true },
+  typeStyle = {},
+  transparent = false,   -- do not set background color
+  dimInactive = false,   -- dim inactive window `:h hl-NormalNC`
+  terminalColors = true, -- define vim.g.terminal_color_{0,17}
+  colors = {             -- add/modify theme and palette colors
+    palette = {},
+    theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+  },
+  overrides = function(colors) -- add/modify highlights
+    return {}
+  end,
+  theme = "dragon",  -- Load "wave" theme when 'background' option is not set
+  background = {     -- map the value of 'background' option to a theme
+    dark = "dragon", -- try "dragon" !
+    light = "dragon"
+  },
+})
+
 local auto_dark_mode = require('auto-dark-mode')
 
 auto_dark_mode.setup({
   update_interval = 3000,
   set_dark_mode = function()
     vim.cmd('colorscheme catppuccin-frappe')
+    --vim.cmd("colorscheme kanagawa")
   end,
   set_light_mode = function()
     vim.cmd('colorscheme catppuccin-latte')
