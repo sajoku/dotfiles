d2d4bd36b3a7619bef40889e0a04b3f99cbf22a1
sander <sander@muliphen.nl>
2024-12-09 16:17:08 +0100

    Setup symlinks from home to dotfiles
    - Making sure I can implode my machine and recreate it easily

--- setup.sh
+++ setup.sh
@@ -3,3 +3,21 @@
 echo "Bundling up homebrew installed apps"
 brew bundle --file=~/.dotfiles/homebrew/.Brewfile
 
+
+echo "Setup symlinks in home"
+ln -s  ~/dotfiles/ackrc ~/.ackrc
+ln -s  ~/dotfiles/agignore ~/.agignore
+ln -s  ~/dotfiles/gitgignore_global ~/.gitignore_global
+ln -s  ~/dotfiles/.irbc ~/irbrc
+ln -s  ~/dotfiles/pdbrc ~/.pdbrc
+ln -s  ~/dotfiles/pryrc ~/.pryrc
+ln -s  ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s  ~/dotfiles/vimrc ~/.vimrc
+ln -s  ~/dotfiles/homebrew/Brewfile ~/.Brewfile
+
+
+echo "Setup symlinks in home .config"
+ln -s  ~/dotfiles/bat/ ~/.config/bat
+ln -s  ~/dotfiles/fish/ ~/.config/fish
+ln -s  ~/dotfiles/starship.toml ~/.config/starship.toml
+ln -s  ~/dotfiles/config/wezterm/ ~/.config/wezterm
