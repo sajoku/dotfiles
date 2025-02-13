dc7ef1b63c90ea2d55647ea2e39c68140642b9ad
sander <sander@muliphen.nl>
2025-02-13 13:42:15 +0100

    Add rose-pine
    - Add rose pine theme. Has a bit better contrast on light and sunshine on the window

--- vim/config.lua
+++ vim/config.lua
@@ -69,9 +69,59 @@ require("catppuccin").setup({
     -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
   },
 })
+require("rose-pine").setup({
+  variant = "auto",      -- auto, main, moon, or dawn
+  dark_variant = "main", -- main, moon, or dawn
+  dim_inactive_windows = false,
+  extend_background_behind_borders = true,
+
+  enable = {
+    terminal = true,
+    legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+    migrations = true,        -- Handle deprecated options automatically
+  },
+
+  styles = {
+    bold = true,
+    italic = true,
+    transparency = false,
+  },
+
+  groups = {
+    border = "muted",
+    link = "iris",
+    panel = "surface",
+
+    error = "love",
+    hint = "iris",
+    info = "foam",
+    note = "pine",
+    todo = "rose",
+    warn = "gold",
+
+    git_add = "foam",
+    git_change = "rose",
+    git_delete = "love",
+    git_dirty = "rose",
+    git_ignore = "muted",
+    git_merge = "iris",
+    git_rename = "pine",
+    git_stage = "iris",
+    git_text = "rose",
+    git_untracked = "subtle",
+
+    h1 = "iris",
+    h2 = "foam",
+    h3 = "rose",
+    h4 = "gold",
+    h5 = "pine",
+    h6 = "foam",
+  },
+})
+
 require('lualine').setup {
   options = {
-    theme = "catppuccin"
+    theme = "rose-pine"
   }
 }
 
@@ -80,10 +130,11 @@ local auto_dark_mode = require('auto-dark-mode')
 auto_dark_mode.setup({
   update_interval = 3000,
   set_dark_mode = function()
-    vim.cmd('colorscheme xcodedark')
+    vim.cmd('colorscheme rose-pine-moon')
   end,
   set_light_mode = function()
-    vim.cmd('colorscheme xcode')
+    --vim.cmd('colorscheme xcodelight')
+    vim.cmd('colorscheme rose-pine-dawn')
   end,
 })
 
