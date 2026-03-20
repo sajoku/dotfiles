993e1db50cb60aada361203f7ebbf1155e3ba7f1
sander <sander@muliphen.nl>
2026-03-20 09:57:22 +0100

    Update fish config
    - Uses a bit more default fish syntax
    - initializes some startup things in the is-interactive shell

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,46 +1,36 @@
-#silence the welcome message
+# Silence the welcome message
 set -g fish_greeting
 
 if status is-interactive
-  # Commands to run in interactive sessions can go here
-  set -gx DEFAULT_USER "sajoku"
+    set -gx DEFAULT_USER "sajoku"
+    set -gx LC_ALL "en_US.UTF-8"
+    set -gx LANG "en_US.UTF-8"
+    set -gx VISUAL nvim
+    set -gx EDITOR $VISUAL
+    set -gx BUNDLER_EDITOR nvim
+    set -gx FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow -g "!{.git,node_modules,vendor/bundle,.venv,.tox,coverage,tmp,.yarn,}/*" -g "!{.DS_Store,.keep}" 2> /dev/null'
+    set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+    set -gx SSH_AUTH_SOCK $HOME/.1password/agent.sock
 
-  set -gx LC_ALL "en_US.UTF-8"
-  set -gx LANG "en_US.UTF-8"
+    if test -f $HOME/.config/fish/aliases.fish
+        source $HOME/.config/fish/aliases.fish
+    end
 
-  set -gx VISUAL nvim
-  set -gx EDITOR $VISUAL
-  set -gx BUNDLER_EDITOR nvim
-
-
-  set -gx FZF_DEFAULT_COMMAND 'rg --files --no-ignore --hidden --follow -g "!{.git,node_modules,vendor/bundle,.venv,.tox,coverage,tmp,.yarn,}/*" -g "!{.DS_Store,.keep}" 2> /dev/null'
-  set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
-
-  # Aliases
-  if [ -f $HOME/.config/fish/aliases.fish ]
-    source $HOME/.config/fish/aliases.fish
-  end
+    pyenv init - | source
+    starship init fish | source
+    mise activate fish | source
 end
 
-if status --is-login
-  set -gx PATH $PATH "$HOME/dotfiles/bin"
-  set -gx PATH $PATH "$HOME/.bin:/usr/local/sbin"
-  set -gx PATH $PATH "$HOME/.bin:/usr/local/bin"
-  set -gx PATH $PATH "$HOME/.local/bin:/usr/local/bin"
-  set -gx PATH $PATH "$HOME/.cargo/bin"
-  set -gx PATH $PATH ".git/safe/../../bin"
-  set -gx PATH $PATH "$HOME/go/bin"
+if status is-login
+    # Homebrew first so it takes priority
+    fish_add_path /opt/homebrew/bin
+    fish_add_path /opt/homebrew/opt/openssl@3/bin
+    fish_add_path $HOME/dotfiles/bin
+    fish_add_path $HOME/.bin
+    fish_add_path $HOME/.local/bin
+    fish_add_path $HOME/.cargo/bin
+    fish_add_path $HOME/go/bin
+    fish_add_path $HOME/.config/yarn/global/node_modules/.bin
 
-  set -gx PATH $PATH "/opt/homebrew/bin"
-  set -gx PATH $PATH "/opt/homebrew/opt/openssl@3/bin"
-  set -gx PATH $PATH $HOME"/.config/yarn/global/node_modules/.bin"
-
-  #Make sure we always run with an active tmux session
-  tmux_chooser
+    tmux_chooser
 end
-
-set -x SSH_AUTH_SOCK $HOME/.1password/agent.sock
-
-pyenv init - | source
-starship init fish | source
-mise activate fish | source
