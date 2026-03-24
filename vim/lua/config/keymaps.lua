95a0b915cc8005abed0884ddcf7513300ebb2567
sander <sander@muliphen.nl>
2026-03-24 11:30:14 +0100

    Remove unused pacakges and config dirs
    - Spring cleanup; removing unused packages and configurations.
    - The goal is a somewhat simpler setup for the upcoming neovim 0.12 update which has a built in package manager.

--- vim/lua/config/keymaps.lua
+++ vim/lua/config/keymaps.lua
@@ -5,17 +5,6 @@ local opts = { noremap = true, silent = true }
 map("n", "<leader>n", ":NERDTreeToggle<CR>", opts)
 map("n", "<leader>f", ":NERDTreeFind<CR>", opts)
 
--- EasyAlign
-map("n", "<leader>ea", ":EasyAlign<CR>", opts)
-map("x", "<leader>ea", ":EasyAlign<CR>", opts)
-
--- Movement + insert
--- map("i", "<C-e>", "<C-o>$", opts)
--- map("i", "<C-a>", "<C-o>^", opts)
--- map("x", "s", "S", opts)
--- map("i", "jj", "<Esc>", opts)
--- map("n", "go", "o<Esc>", opts)
-
 -- Copy filepath
 map("n", "<Leader>cp", [[:let @+=expand("%:p")<CR>]], opts)
 
@@ -33,8 +22,6 @@ map("n", "<leader>a", ":TestSuite<CR>", opts)
 map("n", "<leader>l", ":TestLast<CR>", opts)
 map("n", "<leader>g", ":TestVisit<CR>", opts)
 
--- Git
-map("n", "<leader>G", ":Git commit<CR>", opts)
 
 -- Celery
 map("n", "<leader>c", [[ofrom celery.contrib import rdb;rdb.set_trace()<Esc>]], opts)
